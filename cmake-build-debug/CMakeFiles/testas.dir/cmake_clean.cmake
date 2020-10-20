b3e26e27a0c9f6233d792c14ecda0d3aabe2f7e4
marius <m.gzegozevskis@gmail.com>
2020-10-20 18:09:55 +0300

    Pirmasis mano Git veiksmas

---
+++ cmake-build-debug/CMakeFiles/testas.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testas.dir/main.cpp.obj"
+  "libtestas.dll.a"
+  "testas.exe"
+  "testas.exe.manifest"
+  "testas.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
